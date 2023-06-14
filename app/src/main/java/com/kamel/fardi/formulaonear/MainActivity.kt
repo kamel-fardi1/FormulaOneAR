6e271d43af09e97768b3f689a80260b097ab3c5f
kamel fardi <78621877+kamel-fardi1@users.noreply.github.com>
2023-06-14 14:41:38 +0100

    sceneform setup

--- app/src/main/java/com/kamel/fardi/formulaonear/MainActivity.kt
+++ app/src/main/java/com/kamel/fardi/formulaonear/MainActivity.kt
@@ -2,10 +2,18 @@ package com.kamel.fardi.formulaonear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Load model.glb from assets folder or http url
+        val arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+        arFragment.setOnTapPlaneGlbModel("formula1_alpine.glb")
+        
+        //        arFragment.setOnTapPlaneGlbModel("https://github.com/codemaker2015/3d-models/blob/master/glb_files/model.glb?raw=true")
     }
+
 }
